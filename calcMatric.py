Corrected average precision calculation

calcMatric.py
@@ -1,7 +1,6 @@
-import xml.etree.cElementTree as ET
-from operator import itemgetter
-import os
 import numpy as np
+import os
+import xml.etree.cElementTree as ET
 
 predictedFolder = 'VOC2007/test/outputAnnotations/'
 groundTruthFolder = '../VOC2007/test/Annotations/'
@@ -9,143 +8,147 @@ groundTruthFolder = '../VOC2007/test/Annotations/'
 numOfClasses = 20
 
 
-classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow",
-           "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car",
+           "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike",
+           "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
 
 IoUThreshold = 0.5
 
 
+# Function to calculate mAP
 def calculate_mAP():
 
+    # Init
     fileList = os.listdir(groundTruthFolder)
     truePositives = np.zeros(numOfClasses)
     falsePositives = np.zeros(numOfClasses)
     falseNegatives = np.zeros(numOfClasses)
 
-    iteration = 0
+    # For each image
     for file in fileList:
 
-        predictedFilePath = predictedFolder + file
-        groundTruthFilePath = groundTruthFolder + file
+        # File paths
+        predictedFilePath = os.path.join(predictedFolder, file)
+        groundTruthFilePath = os.path.join(groundTruthFolder, file)
 
-        xmlParsePredicted = ET.parse(predictedFilePath)
-        xmlParseGT = ET.parse(groundTruthFilePath)
+        # Loading Predictions and Ground Truths
+        predictedObject = ET.parse(predictedFilePath).findall('object')
+        groundTruthObject = ET.parse(groundTruthFilePath).findall('object')
 
-        predictedObject = xmlParsePredicted.findall('object')
-        groundTruthObject = xmlParseGT.findall('object')
-
+        # Init dictionaries to save predictions and ground truth per class
+        dictConfidences = {}
         dictPredicted = {}
         dictGT = {}
         dictMask = {}
-        for i in range(numOfClasses):
-            dictPredicted[i] = []
-            dictGT[i] = []
-            dictMask[i] = []
-        # extract the predicted boxes from xml file
+        for classIdx in range(numOfClasses):
+            dictPredicted[classIdx] = []
+            dictGT[classIdx] = []
+            dictMask[classIdx] = []
+
+        # Extract the predicted boxes from xml file
         for item in predictedObject:
-            category = item.find('name').text
-            categoryIndex = classes.index(category)
+            itemClass = item.find('name').text
+            classIdx = classes.index(itemClass)
             confidence = float(item.find('confidence').text)
             xmin = int(item.find('bndbox').find('xmin').text)
             xmax = int(item.find('bndbox').find('xmax').text)
             ymin = int(item.find('bndbox').find('ymin').text)
             ymax = int(item.find('bndbox').find('ymax').text)
+            # Append Confidences
+            dictConfidences[classIdx].append(confidence)
+            # Append dictPredicted
+            dictPredicted[classIdx].append(
+                [xmin, xmax, ymin, ymax])
 
-            dictPredicted[categoryIndex].append(
-                [confidence, xmin, xmax, ymin, ymax])
+        # Sort each of the dictPredicted values in decreasing order of
+        # confidence values
+        for classIdx in range(numOfClasses):
+            dictPredicted[classIdx] = [pred for (c, pred) in sorted(
+                zip(dictConfidences[classIdx], dictPredicted[classIdx]), reverse=True)]
 
-        # if no object is detected, count it as False Negative
-        # if not bool(dictPredicted):
-        #    falseNegatives+=1
-
-        # sort each of the entries by decreasing order of confidence values
-        for i in range(numOfClasses):
-            dictPredicted[i] = sorted(
-                dictPredicted[i], key=itemgetter(0), reverse=True)
-
-        # extract the ground truth boxes from xml file
+        # Extract the ground truth boxes from xml file
         for item in groundTruthObject:
-            category = item.find('name').text
-            categoryIndex = classes.index(category)
-
+            itemClass = item.find('name').text
+            classIndex = classes.index(itemClass)
             xmin = int(item.find('bndbox').find('xmin').text)
             xmax = int(item.find('bndbox').find('xmax').text)
             ymin = int(item.find('bndbox').find('ymin').text)
             ymax = int(item.find('bndbox').find('ymax').text)
+            # Append dictGT
+            dictGT[classIndex].append([xmin, xmax, ymin, ymax])
+            # To find out if a ground truth exists for an object of a class,
+            # and if a prediction has been made corresponding to that object,
+            # we shall append dictMask with 0 if GT exists,
+            # and modify it to 1 when a prediction corresponds with it
+            dictMask[classIndex].append(0)
 
-            dictGT[categoryIndex].append([xmin, xmax, ymin, ymax])
-
-            # 0/1 denotes weather the ground truth has been alloted to a
-            # prediction
-            dictMask[categoryIndex].append(0)
-
-        # for a particular class, say chair
-        for i in xrange(numOfClasses):
-            # if atleast one of the box is predicted to be positive
-            while dictPredicted[i] != []:
+        # For each class
+        for classIdx in range(numOfClasses):
+            # For each predicted bounding box
+            for predBndbox in dictPredicted[classIdx]:
+                # To find the ground truth bounding box corresponding with the
+                # predicted bounding box
                 maxIoU = 0
                 maxIndex = -1
-                for j in range(len(dictGT[i])):
-                    areaMetric = IoU(dictPredicted[i][0][1:], dictGT[i][j])
-                    # print areaMetric,dictPredicted[i][0][1:],dictGT[i][j]
+                # For each ground truth bounding box
+                for GTbndboxIdx in range(len(dictGT[classIdx])):
+                    # Find IoU
+                    areaMetric = IoU(dictPredicted[classIdx][predBndbox],
+                                     dictGT[classIdx][GTbndboxIdx])
+                    # Record that GT bounding box which has maximum IoU with
+                    # the predicted bounding box
                     if areaMetric > maxIoU:
-                        # print areaMetric
+                        # print(areaMetric)
                         maxIoU = areaMetric
-                        maxIndex = j
+                        maxIndex = GTbndboxIdx
+                # If the IoU between the predicted and GT bounding boxes is
+                # greater than IoUThreshold
                 if maxIoU > IoUThreshold:
-                    # if the object has not been detected before
-                    if dictMask[i][maxIndex] == 0:
-                        dictMask[i][maxIndex] = 1
-                        truePositives[i] += 1
+                    # If the object has not been detected before,
+                    if dictMask[classIdx][maxIndex] == 0:
+                        # Modify dictMask to indicate object has been detected
+                        dictMask[classIdx][maxIndex] = 1
+                        # This is a true positive
+                        truePositives[classIdx] += 1
+                    # Else if object has been detected before, since we know
+                    # that the current prediction has lesser confidence than the
+                    # previous one, so we will consider this a false positive
                     else:
-                        falsePositives[i] += 1
+                        falsePositives[classIdx] += 1
+                # If the IoU between the predicted and GT bounding boxes is
+                # lesser than IoUThreshold, we will consider it a
+                # false positive
                 else:
-                    falsePositives[i] += 1
-                dictPredicted[i].pop(0)
+                    falsePositives[classIdx] += 1
+            # FALSE NEGATIVES
+            # For those classes with GT available but no prediction made,
+            # we will consider this a false negative
+            falseNegatives[
+                classIdx] += sum([1 - j for j in dictMask[classIdx]])
 
-        for i in xrange(numOfClasses):
-            falseNegatives[i] += sum([1 - j for j in dictMask[i]])
+    print('True positives in each class:', truePositives)
+    print('False positives in each class:', falsePositives)
+    print('False negatives in each class:', falseNegatives)
 
-        # print 'fileName'+str(file)
-        # print 'truePositives'+str(truePositives)
-        # print 'falsePositives'+str(falsePositives)
-        # print 'falseNegatives'+str(falseNegatives)
+    # Precision in each class
+    precisions = truePositives / (truePositives + falsePositives)
+    print('Precisions in each class: ', precisions)
 
-    print truePositives
-    print falsePositives
-    print falseNegatives
-
-    # print truePositives/(truePositives+falsePositives)
-    # print truePositives/(truePositives+falseNegatives)
-
-    fP = np.sum(falsePositives)
-    tP = np.sum(truePositives)
-    fN = np.sum(falseNegatives)
-    precision = tP / (tP + fP)
-    recall = tP / (tP + fN)
-    print precision
-    print recall
-
-    '''
-    avgPrecision =0
-    for th in xrange(0.0,1.0,0.1):
-        p = np.maximum(precision(recall>=th))
-        avgPrecision+=(p/11)
-    print avgPrecision
-    '''
+    # Average Precision across all classes
+    avgPrecision = np.mean(precisions)
+    print('mAP =', avgPrecision)
 
 
+# A function to calculate Intersection over Union (IoU)
+# i.e. fraction of common area between two boxes
 def IoU(boxA, boxB):
-
     intersectionX = max(0, min(boxA[1], boxB[1]) - max(boxA[0], boxB[0]))
     intersectionY = max(0, min(boxA[3], boxB[3]) - max(boxA[2], boxB[2]))
-
     intersection = intersectionX * intersectionY
     union = (boxA[1] - boxA[0]) * (boxA[3] - boxA[2]) + \
         (boxB[1] - boxB[0]) * (boxB[3] - boxB[2]) - intersection
-    # print intersection,union,intersection*1.0/union
-    return intersection * 1.0 / union
-
+    # print(intersection, union, intersection * 1.0 / union)
+    return intersection * 1. / union
 
 if __name__ == '__main__':
     calculate_mAP()

preprocess.py
@@ -1,10 +1,10 @@
-import xml.etree.ElementTree as ET
-import os
-import numpy as np
+import cPickle
 import cv2
 import math
-import cPickle
+import numpy as np
+import os
 import sys
+import xml.etree.ElementTree as ET
 
 indexFile = '../VOC2007/train/ImageSets/Main/train.txt'
 
@@ -55,6 +55,7 @@ def preprocess_dataset():
         # find the annotations of objects in the list
         xmlObject = xmlParse.findall('object')
         for item in xmlObject:
+
             # per object per image
             totalObjects += 1
             category = item.find('name').text
@@ -63,8 +64,7 @@ def preprocess_dataset():
             xmax = int(item.find('bndbox').find('xmax').text)
             ymin = int(item.find('bndbox').find('ymin').text)
             ymax = int(item.find('bndbox').find('ymax').text)
-
-            # print xmin,xmax,ymin,ymax
+            # print(xmin, xmax, ymin, ymax)
 
             # take the midpoint and scale according to new size
             xC = ((xmin + xmax) * 1.0 / 2.0) * \
@@ -73,8 +73,8 @@ def preprocess_dataset():
                 (imageHeight * 1.0 / originalImageHeight)
             w = (xmax - xmin) * 1.0
             h = (ymax - ymin) * 1.0
+            # print((ymin + ymax) / 2.0, (imageHeight * 1.0 / originalImageHeight))
 
-            # print (ymin+ymax)/2.0, (imageHeight*1.0/originalImageHeight)
             # take xC and yC as offsets to the original image
             xOffset = ((xC % widthOfGrid) * 1.0) / widthOfGrid
             yOffset = ((yC % heightOfGrid) * 1.0) / heightOfGrid
@@ -83,8 +83,8 @@ def preprocess_dataset():
             # i think it should bot be in preprocessing
             wSqrt = math.sqrt(w * 1.0) / originalImageWidth
             hSqrt = math.sqrt(h * 1.0) / originalImageHeight
-            #w = (w*1.0)/originalImageWidth
-            #h = (h*1.0)/originalImageHeight
+            # w = (w * 1.0) / originalImageWidth
+            # h = (h * 1.0) / originalImageHeight
 
             boxData = [xOffset, yOffset, wSqrt, hSqrt]
 
@@ -97,9 +97,7 @@ def preprocess_dataset():
             responsibleGridY = int(yC / heightOfGrid)
             responsibleGrid = np.zeros([7, 7])
 
-            # print
-            # xmin,xmax,ymin,ymax,originalImageWidth,originalImageHeight,xC,yC,w,h,responsibleGridX,
-            # responsibleGridY
+            # print(xmin, xmax, ymin, ymax, originalImageWidth, originalImageHeight, xC, yC, w, h, responsibleGridX, responsibleGridY)
 
             responsibleGrid[responsibleGridX][responsibleGridY] = 1
             responsibleGrid = np.reshape(responsibleGrid, [49])
@@ -112,17 +110,18 @@ def preprocess_dataset():
         # Print something after processing 5 percent of files
         linesProcessed = linesProcessed + 1
         if linesProcessed % 100 == 0:
-            print 'Processed ' + str(linesProcessed) + ' images out of ' + str(numLines)
+            print('Processed ' + str(linesProcessed) + ' images out of ' + str(numLines))
             fileName = 'data/data-' + str(linesProcessed / 100) + '.pkl'
 
             with open(fileName, 'w') as f:
                 cPickle.dump(data, f)
-            print 'Saved ' + fileName
+            
+            print('Saved ' + fileName)
             del data[:]
 
     fileHandle.close()
-    print totalObjects
-    print totalImages
+    print('totalObjects = ' + totalObjects)
+    print('totalImages = ' + totalImages)
 
 if __name__ == "__main__":
     preprocess_dataset()

yolo.py
@@ -1,16 +1,15 @@
-import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
-from six.moves import cPickle as pickle
-import xml.etree.cElementTree as ET
-from xml.dom import minidom
-
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 import random
 import os
 import sys
+import tensorflow as tf
 import time
+import xml.etree.cElementTree as ET
 
+from six.moves import cPickle as pickle
+from xml.dom import minidom
 
 # build a grpah for YOLO tiny architecture
 # define a general conv layer, pooling layer and fc layer
@@ -47,7 +46,7 @@ class Yolo:
     mode = 'testLive'
 
     # bounding box seeds
-    seed = [random.randint(1, 1000) for i in xrange(3)]
+    seed = [random.randint(1, 1000) for i in range(3)]
     # test on a database
     if mode == 'testDB':
         saveAnnotatedImage = True
@@ -152,7 +151,7 @@ class Yolo:
     # Builds the computational graph for the network
     def build_graph(self):
         if self.displayConsole:
-            print "Building Yolo Graph...."
+            print('Building Yolo Graph....')
         self.x = tf.placeholder('float32', [None, 448, 448, 3])
 
         self.conv1 = self.conv_layer(1, self.x, 64, 7, 2)
@@ -196,7 +195,7 @@ class Yolo:
         self.conv23 = self.conv_layer(27, self.conv22, 1024, 3, 1)
         self.conv24 = self.conv_layer(28, self.conv23, 1024, 3, 1)
 
-        # print self.conv24.get_shape()
+        # print(self.conv24.get_shape())
         # size reduced to 1024x7x7
         self.fc1 = self.fc_layer(29, self.conv24, 512,
                                  flatten=True, linear=False)
@@ -212,7 +211,7 @@ class Yolo:
         self.saver.restore(self.sess, self.weightFile)
 
         if self.displayConsole:
-            print 'Loading Complete \n'
+            print('Loading Complete \n')
 
     # complete the training function
     def train_network(self):
@@ -220,14 +219,14 @@ class Yolo:
         # save the weights after each epoch
         if self.trainData:
             if self.displayConsole:
-                print 'Started training...'
+                print('Started training...')
 
             for epoch in range(135):
                 pass
                 # save the model
         else:
             if self.displayConsole:
-                print 'No train data available'
+                print('No train data available')
 
     def calculate_loss_function(self, predicted, groundTruth):
         '''
@@ -332,7 +331,7 @@ class Yolo:
         bias = tf.Variable(tf.constant(0.1, shape=[numberOfFilters]))
 
         padSize = sizeOfFilter // 2
-        # print inputMatrix.get_shape()
+        # print(inputMatrix.get_shape())
         #paddedInput = np.lib.pad(inputMatrix,((0,0),(padSize,padSize),(padSize,padSize),(0,0)),'constant', constant_values = (0,0))
         paddedInput = tf.pad(
             inputMatrix, ([[0, 0], [padSize, padSize], [padSize, padSize], [0, 0]]))
@@ -342,14 +341,14 @@ class Yolo:
         conv_bias = tf.add(conv, bias, name=str(index) + '_conv')
 
         if self.displayConsole:
-            print ' Layer %d Type: Conv Size: %dx%d Stride: %d No.Filters: %d Input Channels : %d' % (index, sizeOfFilter, sizeOfFilter, stride, numberOfFilters, numberOfChannels)
+            print(' Layer %d Type: Conv Size: %dx%d Stride: %d No.Filters: %d Input Channels : %d' % (index, sizeOfFilter, sizeOfFilter, stride, numberOfFilters, numberOfChannels))
         # leaky relu as mentioned in YOLO paper
         return tf.maximum(self.alpha * conv_bias, conv_bias, name=str(index) + '_leaky_relu')
 
     def maxpool_layer(self, index, inputMatrix, sizeOfFilter, stride):
 
         if self.displayConsole:
-            print ' Layer %d Type: Maxpool Size: %dx%d Stride: %d' % (index, sizeOfFilter, sizeOfFilter, stride)
+            print(' Layer %d Type: Maxpool Size: %dx%d Stride: %d' % (index, sizeOfFilter, sizeOfFilter, stride))
         maxpool = tf.nn.max_pool(inputMatrix, ksize=[1, sizeOfFilter, sizeOfFilter, 1], strides=[
                                  1, sizeOfFilter, sizeOfFilter, 1], padding='SAME', name=str(index) + '_maxpool')
         return maxpool
@@ -373,7 +372,7 @@ class Yolo:
         bias = tf.Variable(tf.constant(0.1, shape=[outputNeurons]))
 
         if self.displayConsole:
-            print ' Layer %d Type: FullyConnected InSize: %d OutSize %d Linear: %d' % (index, inputDimension, outputNeurons, int(linear))
+            print(' Layer %d Type: FullyConnected InSize: %d OutSize %d Linear: %d' % (index, inputDimension, outputNeurons, int(linear)))
 
         # linear or leaky relu activation
         if linear:
@@ -386,7 +385,7 @@ class Yolo:
     def detect_from_file(self, fileName):
 
         if self.displayConsole:
-            print 'Detecting object from :' + fileName
+            print('Detecting object from :' + fileName)
         imageMatrix = cv2.imread(fileName)
         return self.detect_from_matrix(imageMatrix)
 
@@ -413,11 +412,11 @@ class Yolo:
         '''
         Threshold the confidence for all classes and apply Non-Maximum supression
         '''
-        # print sum(sum(netOutput))
+        # print(sum(sum(netOutput)))
         # to fill in the probability of every class
         classProbability = np.zeros(
             [self.numOfGrids, self.numOfGrids, self.numOfBoxes, self.numOfClasses])
-        # print netOutput[0:980].shape
+        # print(netOutput[0:980].shape)
 
         # this should be called objectProbability
         classConditionalProbability = np.reshape(
@@ -471,14 +470,14 @@ class Yolo:
         filteredBoxes = filteredBoxes[sort]
         filteredClasses = filteredClasses[sort]
 
-        # print sum(filteredProbability)
+        # print(sum(filteredProbability))
         # non-maximum supression
         for i in range(len(filteredBoxes)):
             if filteredProbability[i] == 0:
                 continue
             for j in range(i + 1, len(filteredBoxes)):
                 if self.iou(filteredBoxes[i], filteredBoxes[j]) > self.IoUThreshold:
-                    # print 'Rejecting Box'+str(j)
+                    # print('Rejecting Box' + str(j))
                     filteredProbability[j] = 0.0
 
         filterIoU = np.array(filteredProbability > 0.0, dtype='bool')
@@ -504,7 +503,7 @@ class Yolo:
             w = int(results[i][3])
             h = int(results[i][4])
 
-            # print x,y,w,h,results[i][0]
+            # print(x, y, w, h, results[i][0])
             imageHeight, imageWidth, _ = image.shape
 
             w = w // 2
@@ -520,7 +519,7 @@ class Yolo:
                 3 if not min(y + h - imageHeight, 0) else (y + h)
 
             if self.displayConsole:
-                print 'Class :' + results[i][0] + ',[x,y,w,h] [' + str(x) + ',' + str(y) + ',' + str(w) + ',' + str(h) + '] Confidence :' + str(results[i][5])
+                print('Class : ' + results[i][0] + ', [x, y, w, h] [' + str(x) + ', ' + str(y) + ', ' + str(w) + ', ' + str(h) + '] Confidence : ' + str(results[i][5]))
                 # each class must have a unique color
             color = tuple(
                 [(j * (1 + self.classes.index(results[i][0])) % 255) for j in self.seed])
@@ -538,9 +537,9 @@ class Yolo:
                             5], (xmin + 5, ymin - 8), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
             ''' 
-            cv2.rectangle(image,(x-w,y-h),(x+w,y+h),color,3)
-            cv2.rectangle(image,(x-w,y-h-20),(x+w,y-h),(125,125,125),-1)
-            cv2.putText(image,results[i][0]+': %.2f' % results[i][5],(x-w+5,y-h-7),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,0),1)
+            cv2.rectangle(image, (x - w, y - h), (x + w, y + h), color, 3)
+            cv2.rectangle(image, (x - w, y - h - 20), (x + w, y - h), (125, 125, 125), -1)
+            cv2.putText(image, results[i][0] + ': %.2f' % results[i][5], (x - w + 5, y - h - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
             '''
 
             objectParameters = [results[i][0], xmin,
@@ -559,7 +558,7 @@ class Yolo:
                             3] * 0.5) - max(boxA[1] - boxA[3] * 0.5, boxB[1] - boxB[3] * 0.5))
         intersection = intersectionX * intersectionY
         union = boxA[2] * boxA[3] + boxB[2] * boxB[3] - intersection
-        # print intersection,union,intersection/union
+        # print(intersection, union, intersection / union)
         return intersection / union
 
     def iouTrain(self, boxA, boxB, realBox):