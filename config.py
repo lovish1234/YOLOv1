aa3fa49fbe9f4feff763bd583061ff4b72fb4bfd
voletiv <vikram.voleti@gmail.com>
2017-08-30 17:56:33 +0530

    Made config.py to store YML and tf params

---
+++ config.py
@@ -0,0 +1,11 @@
+# YOLO params
+numOfGridsIn1D=7
+numOfBoxesPerGrid=2
+minClassProbability=0.2
+iouThreshold=0.5
+lambdaCoordinate=5.0
+lambdaNoObject=0.5
+leakyReLUAlpha=0.1
+
+# Tensorflow params
+batchSize=64
