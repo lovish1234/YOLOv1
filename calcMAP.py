bebdfc7d4d997c7b825acd3b64dfebc9a8f024b6
Lovish <lovish1234@gmail.com>
2017-09-07 12:01:09 +0530

    code structure similar to calc_mAP

--- calcMAP.py
+++ calcMAP.py
@@ -31,8 +31,8 @@ def calculate_mAP():
 
     # total numbers of objects predicted can help in calcu
     # - lating recall as it equals TP + FN 
-    totalPredicted = np.zeros(numOfClasses)
-    totalGT = np.zeros(numOfClasses)
+    totalPredicted = np.zeros(numOfClasses, dtype=int)
+    totalGT = np.zeros(numOfClasses, dtype=int)
 
     for file in fileListPredicted:
         
@@ -89,15 +89,20 @@ def calculate_mAP():
             # and if a prediction has been made corresponding to that object,
             # we shall append dictMask with 0 if GT exists,
             # and modify it to 1 when a prediction corresponds with it
-            dictMask[classId][file].append(0)
+            dictMask[classId][file].append(False)
             totalGT[classId]+=1
  
-    truePositives = np.zeros((numOfClasses, 4000))
-    falsePositives = np.zeros((numOfClasses, 4000))
-    falseNegatives = np.zeros((numOfClasses, 4000))
-
+    truePositives = []
+    falsePositives = []
+    falseNegatives = np.zeros(numOfClasses)
    
     for classId in range(numOfClasses):
+        numberOfPredictedObjectsInClass = totalPredicted[classId]
+        truePositives.append(np.zeros(numberOfPredictedObjectsInClass,
+            dtype=np.float64))
+        falsePositives.append(np.zeros(numberOfPredictedObjectsInClass,
+            dtype=np.float64))
+
         for predictedObjectIndex in range(len(dictPredicted[classId])):
         # To find the ground truth bounding box corresponding with the
         # predicted bounding box
@@ -107,13 +112,13 @@ def calculate_mAP():
             maxIndex = -1
             # If no item of classId predicted is present in the ground truth image
             if len(dictGT[classId][predictedItem[1]])==0:
-                falsePositives[classId,predictedObjectIndex]=1
+                falsePositives[classId][predictedObjectIndex]=1
                 continue
              # For each ground truth box            
             for GTObjectIndex in  range(len(dictGT[classId][predictedItem[1]])):
                 # If particular GTbox has already been alloted to predicted box
                 # move to the next box without considering it
-                if dictMask[classId][predictedItem[1]][GTObjectIndex]==1:
+                if dictMask[classId][predictedItem[1]][GTObjectIndex]==True:
                     continue
                 GTItem = dictGT[classId][predictedItem[1]]
                 areaMetric = IoU(GTItem[GTObjectIndex],
@@ -129,18 +134,18 @@ def calculate_mAP():
             # If all the GT box in a particular image are already alloted
             # to predictedBox, add the new predictedBox to fP
             if maxIndex==-1:
-                falsePositives[classId,predictedObjectIndex]=1
+                falsePositives[classId][predictedObjectIndex]=1
                 continue
 
             if maxIoU > IoUThreshold:
 
                 # If the object has not been detected before
-                if dictMask[classId][predictedItem[1]][maxIndex]==0:
+                if dictMask[classId][predictedItem[1]][maxIndex]==False:
                     
                     # Modify dictMask to indicate that object has been 
                     # detected
-                    dictMask[classId][predictedItem[1]][maxIndex]=1
-                    truePositives[classId,predictedObjectIndex]=1
+                    dictMask[classId][predictedItem[1]][maxIndex]=True
+                    truePositives[classId][predictedObjectIndex]=1
                     
                     # predictObject has been attributed to GT Object
                     # move to next predictedObject
@@ -149,37 +154,35 @@ def calculate_mAP():
                     # Else if object has been detected before, since we know
                     # that the current prediction has lesser confidence than the
                     # previous one, so we will consider this a false positive
-                    falsePositives[classId,predictedObjectIndex]=1
+                    falsePositives[classId][predictedObjectIndex]=1
             else:
-                
-                # FALSE NEGATIVES
-                # For those classes with GT available but no prediction made,
-                # we will consider this a false negative
-                    falseNegatives[classId,predictedObjectIndex]=1
-
-    tP = np.cumsum(truePositives, dtype=float, axis=1)
-    fP = np.cumsum(falsePositives, dtype=float, axis=1)
-
-    precision = tP/(tP+fP)
-    recall = tP/totalPredicted[:,None]
-    
+                falsePositives[classId][predictedObjectIndex]=1
+    cumulativePrecision = []
+    cumulativeRecall = []
     averagePrecision = np.zeros(numOfClasses)
 
     # For each class calculate Interpolated Average Precision
     # as given in PASCAL VOC handbook
     for classId in range(numOfClasses):
+
+        for image in dictMask[classId]:
+            falseNegatives[classId]+=sum([ not x for x in
+                dictMask[classId][image]])
+        cumulativePrecision.append(np.divide(np.cumsum(truePositives[classId]),
+            np.cumsum(truePositives[classId])+np.cumsum(falsePositives[classId])))
+        #cumulativePrecision.append(np.divide(np.cumsum(truePositives[classId]),
+            #1 + np.arange(totalPredicted[classId])))
+
+        cumulativeRecall.append(np.cumsum(truePositives[classId])/totalGT[classId])
+
+        classPrecision = np.asarray(cumulativePrecision[-1], dtype=np.float64)
+        classRecall = np.asarray(cumulativeRecall[-1], dtype=np.float64)
         for threshold in range(0,11,1):
             threshold = (threshold/10.0)
 
-            # Precision and recall value for top-K(confidence) elements 
-            classPrecision = precision[classId]
-            classRecall = recall[classId]
-
-            #print (classPrecision[classRecall>=threshold])
-            
             # Get the maximum precision above a particular recall value 
             precisionValues = (classPrecision[classRecall>=threshold])
-            
+
             # If precision is 0 for all the values 
             if precisionValues.shape[0]==0:
                 p=0
@@ -190,11 +193,6 @@ def calculate_mAP():
             # Average precision would be mean of the precision values 
             # taken at these 11 points ( according to VOC handbook)
             averagePrecision[classId]+=(p/11)
-        '''
-        print (classes[classId])
-        print (averagePrecision[classId])
-        '''
-
 
     meanAveragePrecision = np.mean(averagePrecision)
     print ("Mean Average Precision : %.3f" % meanAveragePrecision)
@@ -202,7 +200,8 @@ def calculate_mAP():
             "Total Ground Truth", 
             "Total Predicted", 
             "True Positives", 
-            "False Positives", 
+            "False Positives",
+            "False Negatives",
             "Average Precision") 
  
     for classId in range(numOfClasses):
@@ -211,6 +210,7 @@ def calculate_mAP():
                 len(dictPredicted[classId]),
                 np.sum(truePositives[classId]),
                 np.sum(falsePositives[classId]),
+                falseNegatives[classId],
                 averagePrecision[classId])
 
 # A function to calculate Intersection over Union (IoU)
